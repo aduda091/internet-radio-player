43ffcc4c07dc8c02718b5e905ec82461998bf8bb
Alen Duda <alenduda@gmail.com>
2021-10-26 20:26:07 +0200

    re-add Rijeka station

--- src/constants/stations.js
+++ src/constants/stations.js
@@ -4,6 +4,7 @@ const stations = [
     { name: 'hrt3', url: 'https://22603.live.streamtheworld.com/PROGRAM3AAC.aac' },
     { name: 'pula', url: 'https://21223.live.streamtheworld.com/PULAAAC.aac' },
     { name: 'istra', url: 'https://uk1.streamingpulse.com/ssl/RadioIstra' },
+    { name: 'rijeka', url: 'https://25703.live.streamtheworld.com/RIJEKA_ADP.aac' },
 ];
 
 export default stations;
