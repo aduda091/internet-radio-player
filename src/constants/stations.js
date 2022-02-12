08cd17abaced9656a95d896dc24a47e514b1a55b
Alen Duda <alenduda@gmail.com>
2022-02-12 10:52:29 +0100

    update radio istra url

--- src/constants/stations.js
+++ src/constants/stations.js
@@ -3,7 +3,7 @@ const stations = [
     { name: 'hrt2', url: 'https://21223.live.streamtheworld.com/PROGRAM2AAC.aac' },
     { name: 'hrt3', url: 'https://22603.live.streamtheworld.com/PROGRAM3AAC.aac' },
     { name: 'pula', url: 'https://21223.live.streamtheworld.com/PULAAAC.aac' },
-    { name: 'istra', url: 'https://uk1.streamingpulse.com/ssl/RadioIstra' },
+    { name: 'istra', url: 'http://146.185.18.251:7117/stream' },
     { name: 'rijeka', url: 'https://25703.live.streamtheworld.com/RIJEKA_ADP.aac' },
 ];
 
