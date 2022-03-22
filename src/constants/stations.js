4275d260865f4ee5fffac931661142a837c20277
Alen Duda <alenduda@gmail.com>
2022-03-22 22:30:05 +0100

    comment out istra station

--- src/constants/stations.js
+++ src/constants/stations.js
@@ -3,7 +3,7 @@ const stations = [
     { name: 'hrt2', url: 'https://21223.live.streamtheworld.com/PROGRAM2AAC.aac' },
     { name: 'hrt3', url: 'https://22333.live.streamtheworld.com/PROGRAM3AAC.aac' },
     { name: 'pula', url: 'https://21223.live.streamtheworld.com/PULAAAC.aac' },
-    { name: 'istra', url: 'http://146.185.18.251:7117/stream' },
+//  { name: 'istra', url: 'http://146.185.18.251:7117/stream' },
     { name: 'rijeka', url: 'https://21223.live.streamtheworld.com/RIJEKAAAC.aac' },
 ];
 
