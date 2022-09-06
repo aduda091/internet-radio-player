e4e1ad0fd7cbcf27e2d3c5f99f1fd46f980f94a5
Alen Duda <alenduda@gmail.com>
2022-09-06 16:30:50 +0200

    update station URLs

--- src/constants/stations.js
+++ src/constants/stations.js
@@ -1,10 +1,10 @@
 const stations = [
-    { name: 'hrt1', url: 'https://23543.live.streamtheworld.com/PROGRAM1AAC.aac' },
-    { name: 'hrt2', url: 'https://23543.live.streamtheworld.com/PROGRAM2AAC.aac' },
+    { name: 'hrt1', url: 'https://21223.live.streamtheworld.com/PROGRAM1AAC.aac' },
+    { name: 'hrt2', url: 'https://21223.live.streamtheworld.com/PROGRAM2AAC.aac' },
     { name: 'hrt3', url: 'https://25653.live.streamtheworld.com/PROGRAM3AAC.aac' },
-    { name: 'pula', url: 'https://23543.live.streamtheworld.com/PULAAAC.aac' },
+    { name: 'pula', url: 'https://21223.live.streamtheworld.com/PULAAAC.aac' },
 //  { name: 'istra', url: 'http://146.185.18.251:7117/stream' },
-    { name: 'rijeka', url: 'https://23543.live.streamtheworld.com/RIJEKAAAC.aac' },
+    { name: 'rijeka', url: 'https://21223.live.streamtheworld.com/RIJEKAAAC.aac' },
 ];
 
 export default stations;
